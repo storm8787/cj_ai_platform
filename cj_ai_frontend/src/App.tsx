64de0100c937387935679a57f0d2fc6e2589a4bc
storm8787 <baechu1310@gmail.com>
2025-12-10 17:02:59 +0900

    Update App.tsx

--- cj_ai_frontend/src/App.tsx
+++ cj_ai_frontend/src/App.tsx
@@ -1,9 +1,19 @@
-// src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Index from "./pages/Index";
+import PressReleasePage from "./pages/PressReleasePage";
+import TranslatorPage from "./pages/TranslatorPage";
 
 function App() {
-  return <Index />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />        
+        <Route path="/press-release" element={<PressReleasePage />} />
+        <Route path="/translator" element={<TranslatorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
