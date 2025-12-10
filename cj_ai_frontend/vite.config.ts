290bed10ae7108ae25621162623a09ed03b696db
storm8787 <baechu1310@gmail.com>
2025-12-10 16:59:50 +0900

    Update vite.config.ts

--- cj_ai_frontend/vite.config.ts
+++ cj_ai_frontend/vite.config.ts
@@ -1,7 +1,6 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -10,4 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
