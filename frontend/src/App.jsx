3bfe29a004255412f7ce6092461b2a274dd75107
storm8787 <baechu1310@gmail.com>
2026-01-13 17:34:54 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Dashboard from './pages/Dashboard'
-import PressRelease from './pages/PressRelease'
-import ElectionLaw from './pages/ElectionLaw'
-import NewsViewer from './pages/NewsViewer'
-import NotFound from './pages/NotFound'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard';
+import NewsViewer from './pages/NewsViewer';
+import PressRelease from './pages/PressRelease';
+import ElectionLaw from './pages/ElectionLaw';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/press-release" element={<PressRelease />} />
-        <Route path="/election-law" element={<ElectionLaw />} />
-        <Route path="/news" element={<NewsViewer />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/news" element={<NewsViewer />} />
+          <Route path="/press-release" element={<PressRelease />} />
+          <Route path="/election-law" element={<ElectionLaw />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
