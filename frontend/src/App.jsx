9d88a60c5b4e6677a28977b576505979c7b24848
storm8787 <baechu1310@gmail.com>
2026-01-14 10:52:14 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import NewsViewer from './pages/NewsViewer';
@@ -7,16 +7,14 @@ import ElectionLaw from './pages/ElectionLaw';
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/news" element={<NewsViewer />} />
-          <Route path="/press-release" element={<PressRelease />} />
-          <Route path="/election-law" element={<ElectionLaw />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/news" element={<NewsViewer />} />
+        <Route path="/press-release" element={<PressRelease />} />
+        <Route path="/election-law" element={<ElectionLaw />} />
+      </Routes>
+    </Layout>
   );
 }
 
