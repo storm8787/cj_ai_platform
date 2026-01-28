df1487c59e20487c22b94344e04bf61c7a28bba1
storm8787 <baechu1310@gmail.com>
2026-01-28 17:10:22 +0900

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["'Pretendard'", "-apple-system", "BlinkMacSystemFont", "sans-serif"],
+      },
       colors: {
         primary: {
           50: '#f0fdf4',
