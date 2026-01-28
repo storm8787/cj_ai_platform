746559c5f6d40a328ac835fed78e0688eb49a932
storm8787 <baechu1310@gmail.com>
2026-01-28 23:09:57 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
     allow_credentials=True,
-    allow_methods=["*"],
+    #allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
     expose_headers=["X-Processed-Count", "X-Total-Rows", "X-Total-Cols", "X-Errors"],
 )
